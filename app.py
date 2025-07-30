323ce572fd4e49bd55cd5c00ab9010b028a6b955
Femi James <femijames070@gmail.com>
2025-07-30 08:34:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
 import numpy as np
-from sklearn.ensemble import RandamForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 st.title("Penguine")
