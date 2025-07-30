9b0580d30a19eb1dadf9bc35d322eeccd8ab8df3
Femi James <femijames070@gmail.com>
2025-07-30 09:49:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -30,9 +30,6 @@ inf
 with st.expander("Data Visualization"):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
-  st.subheader("Box Plot: Body Mass Distribution by Species") # Added subheader for clarity
-  st.boxplot(data=df, x='species', y='body_mass_g') # Corrected indentation here)
-
 with st.expander("Input data"):
   pass
 
