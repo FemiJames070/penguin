1ca682803ac3ea15d97045be6977ae81be331076
Femi James <femijames070@gmail.com>
2025-07-30 08:40:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,20 @@ import streamlit as st
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 
-st.title("Penguine")
+st.title("Penguine Spicey Prediction ML app")
+st.info("This is end-to-end Mahcine Learning App")
+
+with st.expander("Data"):
+  pass
+
+with st.expander("Data Visualization"):
+  pass
+
+with st.expander("Input data"):
+  pass
+
+with st.expander("Data Preparation"):
+  pass
+
+with st.sidebar:
+  st.header("Input Variables")
