7ef5bdd1002c439888bebb8e1ce88e18f720e159
Femi James <femijames070@gmail.com>
2026-03-31 11:34:03 -0600

    Refactor data display and input features in app2.py

--- app2.py
+++ app2.py
@@ -6,59 +6,55 @@ import altair as alt
 
 st.title('🤖 Machine Learning App Jazzpreet')
 
-st.info('This is app builds a machine learning model!')
+st.info('This app builds a machine learning model!')
 
 with st.expander('Data'):
-  st.write('**Raw data**')
-  df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
-  df
+    st.write('**Raw data**')
+    df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+    st.dataframe(df)
 
-  st.write('**X**')
-  X_raw = df.drop('species', axis=1)
-  X_raw
+    st.write('**X**')
+    X_raw = df.drop('species', axis=1)
+    st.dataframe(X_raw)
 
-  st.write('**y**')
-  y_raw = df.species
-  y_raw
+    st.write('**y**')
+    y_raw = df.species
+    st.dataframe(y_raw)
 
 with st.expander('Data visualization'):
-  #st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
-  
-
-  chart = alt.Chart(df).mark_circle(size=60).encode(
-    x='bill_length_mm',
-    y='body_mass_g',
-    color='species',
-    tooltip=['species', 'bill_length_mm', 'body_mass_g']
-).interactive()
-  st.altair_chart(chart, use_container_width=True)
+    chart = alt.Chart(df).mark_circle(size=60).encode(
+        x='bill_length_mm',
+        y='body_mass_g',
+        color='species',
+        tooltip=['species', 'bill_length_mm', 'body_mass_g']
+    ).interactive()
+    st.altair_chart(chart, use_container_width=True)
 
 # Input features
 with st.sidebar:
-  st.header('Input features')
-  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
-  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
-  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
-  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
-  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
-  gender = st.selectbox('Gender', ('male', 'female'))
-  
-  # Create a DataFrame for the input features
-  data = {'island': island,
-          'bill_length_mm': bill_length_mm,
-          'bill_depth_mm': bill_depth_mm,
-          'flipper_length_mm': flipper_length_mm,
-          'body_mass_g': body_mass_g,
-          'sex': gender}
-  input_df = pd.DataFrame(data, index=[0])
-  input_penguins = pd.concat([input_df, X_raw], axis=0)
+    st.header('Input features')
+    island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+    bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+    bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+    flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
+    body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+    gender = st.selectbox('Gender', ('male', 'female'))
+    
+    # Create a DataFrame for the input features
+    data = {'island': island,
+            'bill_length_mm': bill_length_mm,
+            'bill_depth_mm': bill_depth_mm,
+            'flipper_length_mm': flipper_length_mm,
+            'body_mass_g': body_mass_g,
+            'sex': gender}
+    input_df = pd.DataFrame(data, index=[0])
+    input_penguins = pd.concat([input_df, X_raw], axis=0)
 
 with st.expander('Input features'):
-  st.write('**Input penguin**')
-  input_df
-  st.write('**Combined penguins data**')
-  input_penguins
-
+    st.write('**Input penguin**')
+    st.dataframe(input_df)
+    st.write('**Combined penguins data**')
+    st.dataframe(input_penguins)
 
 # Data preparation
 # Encode X
@@ -73,16 +69,15 @@ target_mapper = {'Adelie': 0,
                  'Chinstrap': 1,
                  'Gentoo': 2}
 def target_encode(val):
-  return target_mapper[val]
+    return target_mapper[val]
 
 y = y_raw.apply(target_encode)
 
 with st.expander('Data preparation'):
-  st.write('**Encoded X (input penguin)**')
-  input_row
-  st.write('**Encoded y**')
-  y
-
+    st.write('**Encoded X (input penguin)**')
+    st.dataframe(input_row)
+    st.write('**Encoded y**')
+    st.dataframe(y)
 
 # Model training and inference
 ## Train the ML model
@@ -95,9 +90,6 @@ prediction_proba = clf.predict_proba(input_row)
 
 df_prediction_proba = pd.DataFrame(prediction_proba)
 df_prediction_proba.columns = ['Adelie', 'Chinstrap', 'Gentoo']
-df_prediction_proba.rename(columns={0: 'Adelie',
-                                 1: 'Chinstrap',
-                                 2: 'Gentoo'})
 
 # Display predicted species
 st.subheader('Predicted Species')
@@ -125,7 +117,6 @@ st.dataframe(df_prediction_proba,
                  max_value=1
                ),
              }, hide_index=True)
-
 
 penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
 st.success(str(penguins_species[prediction][0]))
